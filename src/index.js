1b06691bfe380f292719474fc210b77590d640ec
sam gardner <samgardner20@gmail.com>
2022-08-26 11:19:11 -0400

    added a try catch block for very basic error handling

--- src/index.js
+++ src/index.js
@@ -2,10 +2,16 @@ import './style.css'
 const APIKEY = '6a05651102c94de61e3f7cf2e46a1338'
 
 async function getCurrentWeather(lat, lon) {
-    const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${APIKEY}`, {mode: 'cors'}
-    )
-    const data = await response.json()
-    console.log(data);
+    try{
+
+        const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${APIKEY}`, {mode: 'cors'}
+        )
+        const data = await response.json()
+        console.log(data);
+        
+    } catch(error){
+        console.log(error);
+    }
 
 }
 

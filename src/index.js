3dc454fce4eb8a6bb1b461af4c0f71a78a1fcc36
sam gardner <samgardner20@gmail.com>
2022-08-26 11:16:18 -0400

    got the first API call to work. need us use the geocoding api to make it more useful

--- src/index.js
+++ src/index.js
@@ -1,1 +1,14 @@
 import './style.css'
+const APIKEY = '6a05651102c94de61e3f7cf2e46a1338'
+
+async function getCurrentWeather(lat, lon) {
+    const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${APIKEY}`, {mode: 'cors'}
+    )
+    const data = await response.json()
+    console.log(data);
+
+}
+
+
+
+getCurrentWeather(51.5085,-0.1257)

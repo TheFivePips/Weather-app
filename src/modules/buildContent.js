e06dba9c8d402e636532d7b54a8ab4a78f050c83
sam gardner <samgardner20@gmail.com>
2022-08-29 10:29:14 -0400

    added in some icons and some very basic date work. still  needs to be adjusted

--- src/modules/buildContent.js
+++ src/modules/buildContent.js
@@ -1,23 +1,52 @@
+import Cloudy from '../../assets/cloudy.svg'
+import PartlyCloudy from '../../assets/partly-cloudy.svg'
+import Rainy from '../../assets/rainy.svg'
+import Sunny from '../../assets/sunny.svg'
+
+import fromUnixTime from 'date-fns/fromUnixTime'
+import format from 'date-fns/format'
+import parseISO from 'date-fns/parseISO'
+
 
 export default function buildContent(myData) {
     const { temp, feelslike, description, humidity, sunrise, sunset } = myData
-    
+
+    const icon = document.getElementById('weather-icon')
+    if(description === "cloudy"){
+        
+        icon.src = Cloudy
+    }
+    if(description === "rainy"){
+        
+        icon.src = Rainy
+    }
+    if(description === "sunny"){
+        
+        icon.src = Sunny
+    }
+    else {
+        icon.src = PartlyCloudy
+    }
+
     const tempSpan = document.getElementById('temp')
     tempSpan.textContent = temp
 
     const feelsLikeSpan = document.getElementById('feels-like')
-    feelsLikeSpan.textContent = feelslike
+    feelsLikeSpan.textContent = ` ${feelslike}`
         
     const descriptionh4 = document.getElementById('description')
     descriptionh4.textContent = description
+    
 
     const humidityh4 = document.getElementById('humidity')
-    humidityh4.textContent = humidity
+    humidityh4.textContent = ` ${humidity} %`
 
     const sunriseSpan = document.getElementById('sunrise')
-    sunriseSpan.textContent = sunrise
+    // console.log(format(parseISO(fromUnixTime(sunrise)), 'p'));
+
+    sunriseSpan.textContent = ` ${fromUnixTime(sunrise).toUTCString().split(' ')[4]}`
 
     const sunsetSpan = document.getElementById('sunset')
-    sunsetSpan.textContent = sunset
+    sunsetSpan.textContent = ` ${fromUnixTime(sunset).toUTCString().split(' ')[4]}`
 }
 

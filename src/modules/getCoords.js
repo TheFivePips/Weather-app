ef9f79fb592751a072bf3c827068216dfd4ca235
sam gardner <samgardner20@gmail.com>
2022-08-29 15:55:37 -0400

    hopefully fiex the mixed content problem

--- src/modules/getCoords.js
+++ src/modules/getCoords.js
@@ -6,7 +6,7 @@ import get5day3HourlyWeather from "./get5day3HourlyWeather";
 export default async function getCoords(cityname, statecode, countrycode){
     try {
 
-        const response = await fetch(`http://api.openweathermap.org/geo/1.0/direct?q=${cityname},${statecode},${countrycode}&limit=1&appid=${APIKEY}`)
+        const response = await fetch(`https://api.openweathermap.org/geo/1.0/direct?q=${cityname},${statecode},${countrycode}&limit=1&appid=${APIKEY}`)
     
         const data = await response.json()
         
